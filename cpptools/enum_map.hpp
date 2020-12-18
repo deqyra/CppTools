5208f2e5909a3fce15e68cca0cc01553ef42714e
deqyra <deqyra@gmail.com>
2020-12-18 10:39:58 +0100

    Add new enum map and multimap definitions

--- cpptools/enum_map.hpp
+++ cpptools/enum_map.hpp
@@ -1,6 +1,7 @@
 #ifndef CPPTOOLS__ENUM_MAP_HPP
 #define CPPTOOLS__ENUM_MAP_HPP
 
+#include <map>
 #include <unordered_map>
 
 namespace std
@@ -15,7 +16,16 @@ namespace std
     };
 
     template<typename Key, typename T>
-    using enum_map = unordered_map<Key, T, EnumClassHash>;
+    using unordered_enum_map = unordered_map<Key, T, EnumClassHash>;
+
+    template<typename Key, typename T>
+    using enum_map = map<Key, T, EnumClassHash>;
+
+    template<typename Key, typename T>
+    using unordered_enum_multimap = unordered_multimap<Key, T, EnumClassHash>;
+
+    template<typename Key, typename T>
+    using enum_multimap = multimap<Key, T, EnumClassHash>;
 }
 
 #endif//CPPTOOLS__ENUM_MAP_HPP
