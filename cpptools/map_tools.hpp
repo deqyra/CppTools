563e2af4ba2f20db6054779d1dfd375d877481ea
François Brachais <francois.brachais@free.fr>
2020-12-22 17:26:23 +0100

    Add map_tools utility

---
+++ cpptools/map_tools.hpp
@@ -0,0 +1,21 @@
+#ifndef CPPTOOLS__MULTIMAP_TOOLS_HPP
+#define CPPTOOLS__MULTIMAP_TOOLS_HPP
+
+#include <map>
+#include <unordered_map>
+#include <utility>
+
+template<typename Key, typename Val, typename Hash, template<typename Key, typename Val, typename Hash> typename T>
+bool mapContainsPair(const T<Key, Val, Hash>& map, std::pair<const Key, Val> pair)
+{
+    using Iter = T<Key, Val, Hash>::iterator;
+    std::pair<Iter, Iter> range = map.equal_range(pair.first);
+    for (Iter it = range.first; it != range.second; it++)
+    {
+        if (it->second == pair.second) return true;
+    }
+
+    return false;
+}
+
+#endif//CPPTOOLS__MULTIMAP_TOOLS_HPP
