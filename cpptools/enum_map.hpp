e18e98f692bda3d759db0a3848997af06be5fcf8
François Brachais <deqyra@gmail.com>
2020-12-24 05:59:35 +0100

    rename EnumHash back to enum_hash as the problem was elsewhere, redo template thing on map tools

--- cpptools/enum_map.hpp
+++ cpptools/enum_map.hpp
@@ -5,29 +5,29 @@
 #include <type_traits>
 #include <unordered_map>
 
-class EnumHash
-{
-    public:
-        template <typename T>
-        std::size_t operator()(T t) const
-        {
-            return static_cast<std::size_t>(t);
-        }
-};
-
 namespace std
 {
-    template<typename Key, typename T>
-    using unordered_enum_map = unordered_map<Key, T, EnumHash>;
+    class enum_hash
+    {
+        public:
+            template <typename T>
+            std::size_t operator()(T t) const
+            {
+                return static_cast<std::size_t>(t);
+            }
+    };
 
     template<typename Key, typename T>
-    using enum_map = map<Key, T, EnumHash>;
+    using unordered_enum_map = unordered_map<Key, T, enum_hash>;
 
     template<typename Key, typename T>
-    using unordered_enum_multimap = unordered_multimap<Key, T, EnumHash>;
+    using enum_map = map<Key, T, enum_hash>;
 
     template<typename Key, typename T>
-    using enum_multimap = multimap<Key, T, EnumHash>;
+    using unordered_enum_multimap = unordered_multimap<Key, T, enum_hash>;
+
+    template<typename Key, typename T>
+    using enum_multimap = multimap<Key, T, enum_hash>;
 }
 
 #endif//CPPTOOLS__ENUM_MAP_HPP
