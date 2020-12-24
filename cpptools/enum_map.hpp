ffc0060d8131fd12dd858eafdf7e9897b2bbd145
François Brachais <francois.brachais@free.fr>
2020-12-24 05:41:08 +0100

    rename enum_hash to EnumHash as it conflicts with names defined by msvc standard implementation

--- cpptools/enum_map.hpp
+++ cpptools/enum_map.hpp
@@ -5,29 +5,29 @@
 #include <type_traits>
 #include <unordered_map>
 
+class EnumHash
+{
+    public:
+        template <typename T>
+        std::size_t operator()(T t) const
+        {
+            return static_cast<std::size_t>(t);
+        }
+};
+
 namespace std
 {
-    class enum_hash
-    {
-        public:
-            template <typename T>
-            std::size_t operator()(T t) const
-            {
-                return static_cast<std::size_t>(t);
-            }
-    };
+    template<typename Key, typename T>
+    using unordered_enum_map = unordered_map<Key, T, EnumHash>;
 
     template<typename Key, typename T>
-    using unordered_enum_map = unordered_map<Key, T, enum_hash>;
+    using enum_map = map<Key, T, EnumHash>;
 
     template<typename Key, typename T>
-    using enum_map = map<Key, T, enum_hash>;
+    using unordered_enum_multimap = unordered_multimap<Key, T, EnumHash>;
 
     template<typename Key, typename T>
-    using unordered_enum_multimap = unordered_multimap<Key, T, enum_hash>;
-
-    template<typename Key, typename T>
-    using enum_multimap = multimap<Key, T, enum_hash>;
+    using enum_multimap = multimap<Key, T, EnumHash>;
 }
 
 #endif//CPPTOOLS__ENUM_MAP_HPP
