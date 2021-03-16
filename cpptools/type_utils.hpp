b2e9ef20628c32fcd2de421828a84575a19200df
François Brachais <francois.brachais@free.fr>
2021-03-16 02:12:52 +0100

    Add type utils

---
+++ cpptools/type_utils.hpp
@@ -0,0 +1,83 @@
+#ifndef CPUEMU__COMMON__TYPE_UTILS_HPP
+#define CPUEMU__COMMON__TYPE_UTILS_HPP
+
+#include <type_traits>
+#include <utility>
+#include <valarray>
+
+namespace type_utils
+{
+
+namespace detail
+{
+
+    struct sfinae_base
+    {
+        using yes = char;
+        using no  = yes[2];
+    };
+
+    template <typename T>
+    struct has_const_iterator : private sfinae_base
+    {
+    private:
+        template <typename C> static yes & test(typename C::const_iterator*);
+        template <typename C> static no  & test(...);
+    public:
+        static const bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
+        using type =  T;
+
+        void dummy(); //for GCC to supress -Wctor-dtor-privacy
+    };
+
+    template <typename T>
+    struct has_begin_end : private sfinae_base
+    {
+    private:
+        template <typename C>
+        static yes & f(typename std::enable_if<
+            std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::begin)),
+                            typename C::const_iterator(C::*)() const>::value>::type *);
+
+        template <typename C> static no & f(...);
+
+        template <typename C>
+        static yes & g(typename std::enable_if<
+            std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::end)),
+                            typename C::const_iterator(C::*)() const>::value, void>::type*);
+
+        template <typename C> static no & g(...);
+
+    public:
+        static bool const beg_value = sizeof(f<T>(nullptr)) == sizeof(yes);
+        static bool const end_value = sizeof(g<T>(nullptr)) == sizeof(yes);
+
+        void dummy(); //for GCC to supress -Wctor-dtor-privacy
+    };
+
+}
+
+template <typename T>
+struct is_container : public std::integral_constant<bool,
+                                                    detail::has_const_iterator<T>::value &&
+                                                    detail::has_begin_end<T>::beg_value  &&
+                                                    detail::has_begin_end<T>::end_value> { };
+
+template <typename T, std::size_t N>
+struct is_container<T[N]> : std::true_type { };
+
+template <std::size_t N>
+struct is_container<char[N]> : std::false_type { };
+
+template <typename T>
+struct is_container<std::valarray<T>> : std::true_type { };
+
+template <typename T1, typename T2>
+struct is_container<std::pair<T1, T2>> : std::true_type { };
+
+template <typename ...Args>
+struct is_container<std::tuple<Args...>> : std::true_type { };
+
+}
+
+#endif//CPUEMU__COMMON__TYPE_UTILS_HPP
