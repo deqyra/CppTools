de5f846b1fb55c6c33d640ab3c1754702425888c
François BRACHAIS <francois.brachais@stormshield.eu>
2021-03-25 19:56:40 +0100

    Add type trait for sized containers in type utils

--- cpptools/type_utils.hpp
+++ cpptools/type_utils.hpp
@@ -37,7 +37,7 @@ namespace detail
         template <typename C>
         static yes & f(typename std::enable_if<
             std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::begin)),
-                            typename C::const_iterator(C::*)() const>::value>::type *);
+                            typename C::const_iterator(C::*)() const>::value>::type*);
 
         template <typename C> static no & f(...);
 
@@ -51,6 +51,23 @@ namespace detail
     public:
         static bool const beg_value = sizeof(f<T>(nullptr)) == sizeof(yes);
         static bool const end_value = sizeof(g<T>(nullptr)) == sizeof(yes);
+
+        void dummy(); //for GCC to supress -Wctor-dtor-privacy
+    };
+
+    template <typename T>
+    struct has_size : private sfinae_base
+    {
+    private:
+        template <typename C>
+        static yes & f(typename std::enable_if<
+            std::is_same<decltype(static_cast<typename C::size_type(C::*)() const>(&C::size)),
+                            typename C::size_type(C::*)() const>::value>::type*);
+
+        template <typename C> static no & f(...);
+
+    public:
+        static bool const value = sizeof(f<T>(nullptr)) == sizeof(yes);
 
         void dummy(); //for GCC to supress -Wctor-dtor-privacy
     };
@@ -77,6 +94,28 @@ struct is_container<std::pair<T1, T2>> : std::true_type { };
 
 template <typename ...Args>
 struct is_container<std::tuple<Args...>> : std::true_type { };
+
+template <typename T>
+struct is_sized_container : public std::integral_constant<bool,
+                                                    detail::has_const_iterator<T>::value &&
+                                                    detail::has_begin_end<T>::beg_value  &&
+                                                    detail::has_begin_end<T>::end_value  &&
+                                                    detail::has_size<T>::value> { };
+
+template <typename T, std::size_t N>
+struct is_sized_container<T[N]> : std::true_type { };
+
+template <std::size_t N>
+struct is_sized_container<char[N]> : std::false_type { };
+
+template <typename T>
+struct is_sized_container<std::valarray<T>> : std::true_type { };
+
+template <typename T1, typename T2>
+struct is_sized_container<std::pair<T1, T2>> : std::true_type { };
+
+template <typename ...Args>
+struct is_sized_container<std::tuple<Args...>> : std::true_type { };
 
 }
 
