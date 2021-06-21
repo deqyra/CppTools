875bc685d8d97d77dcde05403aed4858155bdaaf
François BRACHAIS <francois.brachais@stormshield.eu>
2021-06-21 21:52:49 +0200

    Improve Worker class

--- cpptools/thread/interfaces/interruptible.hpp
+++ cpptools/thread/interfaces/interruptible.hpp
@@ -1,22 +1,62 @@
 #ifndef CPPTOOLS__THREAD__INTERFACES__INTERRUPTIBLE_HPP
 #define CPPTOOLS__THREAD__INTERFACES__INTERRUPTIBLE_HPP
 
+#include <memory>
+
 namespace cpptools
 {
 
+/// @brief Interface for a class implementing an interruptible process.
 class Interruptible
 {
 public:
-    /// @brief Ask for the threaded function to stop processing and cleanly exit.
+    /// @brief Ask for the threaded process to stop processing and cleanly exit.
     virtual void finalize() = 0;
 
-    /// @brief Pause the execution of the threaded function.
+    /// @brief Tells whether or not the threaded process is finalized.
+    virtual bool finalized() = 0;
+
+    /// @brief Hangs the calling thread until the threaded process is finalized.
+    ///
+    /// @param pauseNow Whether or not to request the threaded process to finalize
+    /// upon calling this function. Providing false may prove useful when the
+    /// threaded process must be requested to finalize by another thread than the
+    /// one calling this function, or if the threaded process has already been 
+    /// requested to finalize.
+    virtual void waitUntilFinalized(bool pauseNow = true) = 0;
+
+    /// @brief Pause the execution of the threaded process.
     virtual void pause() = 0;
-    
+
+    /// @brief Tells whether or not the threaded process is paused.
+    virtual bool paused() = 0;
+
+    /// @brief Hangs the calling thread until the threaded process is paused.
+    ///
+    /// @param pauseNow Whether or not to request the threaded process to pause
+    /// upon calling this function. Providing false may prove useful when the
+    /// threaded process must be requested to pause by another thread than the
+    /// one calling this function, or if the threaded process has already been 
+    /// requested to pause.
+    virtual void waitUntilPaused(bool pauseNow = true) = 0;
+
     /// @brief Resume the execution of the threaded function.
-    virtual void resume() = 0;
+    virtual void run() = 0;
+
+    /// @brief Tells whether or not the threaded process is running.
+    virtual bool running() = 0;
+
+    /// @brief Hangs the calling thread until the threaded process is running.
+    ///
+    /// @param pauseNow Whether or not to request the threaded process to pause
+    /// upon calling this function. Providing false may prove useful when the
+    /// threaded process must be requested to resume by another thread than the
+    /// one calling this function, or if the threaded process has already been 
+    /// requested to resume.
+    virtual void waitUntilRunning(bool pauseNow = true) = 0;
 };
 
+using InterruptiblePtr = std::shared_ptr<Interruptible>;
 
 }
 
--- cpptools/thread/worker.hpp
+++ cpptools/thread/worker.hpp
@@ -15,34 +15,76 @@ namespace cpptools
 class Worker : public Interruptible
 {
 private:
-    /// @brief Protection around the state flags.
-    std::mutex _mutex;
+    /// @brief Protection around the execution flow flags (_execute and _exit).
+    std::mutex _mutexExecute;
 
-    /// @brief Conditional to check upon in order to know whether to keep 
-    /// processing or to halt.
-    std::condition_variable _cond;
+    /// @brief Protection around the _finalized flag.
+    std::mutex _mutexFinalized;
 
-    /// @brief Stop processing entirely.
+    /// @brief Protection around the _pause flag.
+    std::mutex _mutexPause;
+
+    /// @brief Protection around the _running flag.
+    std::mutex _mutexRun;
+
+    /// @brief Semaphore to wait by in order to control the execution flow.
+    std::condition_variable _semExecute;
+
+    /// @brief Semaphore to wait by in order to know whether the thread
+    /// is finalized.
+    std::condition_variable _semFinalized;
+
+    /// @brief Semaphore to wait by in order to know whether the thread
+    /// is paused.
+    std::condition_variable _semPause;
+
+    /// @brief Semaphore to wait by in order to know whether the thread
+    /// is running.
+    std::condition_variable _semRunning;
+
+    ///                          ///    \\\ 
+    ///                         /// !!!! \\\ 
+    ///                        ///        \\\ 
+    ///
+    /// _exit and _execute are the only flags which control the execution
+    /// flow of the threaded process. All other boolean flags are only meant
+    /// to be informative.
+
+    /// @brief Stop processing and finalize.
     bool _exit;
 
-    /// @brief Whether to execute or to halt.
+    /// @brief Whether to keep executing or to halt.
     bool _execute;
 
-    /// @brief Function to execute in a loop.
+    /// @brief Whether or not the thread is finalized.
+    bool _finalized;
+
+    /// @brief Whether or not the thread is paused.
+    bool _paused;
+
+    /// @brief Whether or not the thread is running.
+    bool _running;
+
+    ///                          ///    \\\ 
+    ///                         /// !!!! \\\ 
+    ///                        ///        \\\ 
+    ///
+    /// _exit and _execute are the only flags which control the execution
+    /// flow of the threaded process. All other boolean flags are only meant
+    /// to be informative.
+
+    /// @brief Process (function) to execute in a loop.
     std::function<void()> _task;
 
-    /// @brief Handled to the execution thread of the function.
+    /// @brief Handle to the execution thread of the process.
     std::future<void> _thread;
 
-    /// @brief Execute the function in a loop until asked to stop.
+    /// @brief Control the execution of the threaded process using state flags.
     void _work();
 
-    /// @brief Lock and acquire the mutex around state flags.
-    std::unique_lock<std::mutex> _lock();
-
 public:
-    /// @param task Function to execute in a loop.
-    /// @param startNow Whether or not to start the function.
+    /// @param task Process (function) to execute in a loop.
+    /// @param startNow Whether or not to start the process.
     Worker(std::function<void()> t, bool startNow = true);
     ~Worker();
 
@@ -55,14 +97,23 @@ public:
     ///                                       ///
     /////////////////////////////////////////////
 
-    /// @brief Ask for the threaded function to stop processing and cleanly exit.
     virtual void finalize();
 
-    /// @brief Pause the execution of the threaded function.
+    virtual bool finalized();
+
+    virtual void waitUntilFinalized(bool finalizeNow = true);
+
     virtual void pause();
     
-    /// @brief Resume the execution of the threaded function.
-    virtual void resume();
+    virtual bool paused();
+
+    virtual void waitUntilPaused(bool pauseNow = true);
+
+    virtual void run();
+
+    virtual bool running();
+
+    virtual void waitUntilRunning(bool runNow = true);
 };
 
 }//namespace cpptools
