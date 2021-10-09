4460f4d181960d7f3d015d5b349fb460dd99aa10
François BRACHAIS <francois.brachais@stormshield.eu>
2021-10-09 19:47:12 +0200

    Fix bitwise enum ops: wrong usage of std::underlying_type

--- cpptools/utility/bitwise_enum_ops.hpp
+++ cpptools/utility/bitwise_enum_ops.hpp
@@ -19,55 +19,55 @@ concept bitwise_enabled_enum = std::is_enum_v<T> && enable_bitwise_enum_v<T>;
 namespace bitwise_enum_ops
 {
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T operator&(const T& left, const T& right)
     {
         return (T)((i)left & (i)right);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T& operator&=(T& left, const T& right)
     {
         return left = (T)((i)left & (i)right);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T operator|(const T& left, const T& right)
     {
         return (T)((i)left | (i)right);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T& operator|=(T& left, const T& right)
     {
         return left = (T)((i)left | (i)right);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T operator^(const T& left, const T& right)
     {
         return (T)((i)left ^ (i)right);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T& operator^=(T& left, const T& right)
     {
         return left = (T)((i)left ^ (i)right);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     T operator~(const T& value)
     {
         return (T)(~(i)value);
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     bool none(const T& value)
     {
         return (i)value == (i)0;
     }
 
-    template<bitwise_enabled_enum T, typename i = std::underlying_type<T>>
+    template<bitwise_enabled_enum T, typename i = std::underlying_type_t<const T>>
     bool any(const T& value)
     {
         return !none(value);
