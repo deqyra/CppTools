2e460bb4929d440072c41f5953b9296065840bc6
François Brachais <francois.brachais@free.fr>
2022-09-01 14:58:02 +0200

    Debug facilities

---
+++ cpptools/_internal/debug_log.cpp
@@ -0,0 +1,31 @@
+#include "debug_log.hpp"
+
+#include <iostream>
+#include <ostream>
+
+namespace cpptools::internal::debug_log
+{
+
+namespace
+{
+    std::ostream& _out = std::cerr;
+    const char* _level_names[] = {
+        "none",
+        "critical",
+        "pedantic",
+        "extra"
+    };
+}
+
+std::map<std::string_view, level> channel_levels = {};
+
+void log(level lv, std::string_view channel, std::string_view message)
+{
+    auto it = channel_levels.find(channel);
+    if (it != channel_levels.end() && (lv <= it->second))
+    {
+        _out << '[' << _level_names[lv] << "][" << channel << "] " << message << std::endl;
+    }
+}
+
+}
