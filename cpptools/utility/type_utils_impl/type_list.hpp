ef6dfcce3ba9ad16ebd5e8b4bbedaf571af7b8d1
François BRACHAIS <francois.brachais@stormshield.eu>
2022-09-01 15:02:18 +0200

    Better type traits, renamed to type utils

--- cpptools/utility/type_utils_impl/type_list.hpp
+++ cpptools/utility/type_utils_impl/type_list.hpp
@@ -1,7 +1,7 @@
-#ifndef TOOLS__UTILITY__TYPE_IMPL__TYPE_LIST_HPP
-#define TOOLS__UTILITY__TYPE_IMPL__TYPE_LIST_HPP
+#ifndef CPPTOOLS__UTILITY__TYPE_TRAITS_IMPL__TYPE_LIST_HPP
+#define CPPTOOLS__UTILITY__TYPE_TRAITS_IMPL__TYPE_LIST_HPP
 
-namespace tools::type
+namespace cpptools::type_utils
 {
 
 /////////////////////////
@@ -44,30 +44,37 @@ struct make_type_list<>
     using type = type_list<>;
 };
 
-// Specialisation combining 2 type_lists
+// Specialisation to end recursion on a type list
+template<typename... AllTypes>
+struct make_type_list<type_list<AllTypes...>>
+{
+    using type = type_list<AllTypes...>;
+};
+
+// Specialisation to end recursion on a regular type
+template<typename T>
+struct make_type_list<T>
+{
+    using type = type_list<T>;
+};
+
+// Specialisation for combining 2 type_lists
 template<typename... Head, typename... Tail>
-struct make_type_list<
-    type_list<Head...>,
-    type_list<Tail...>
->
+struct make_type_list<type_list<Head...>, type_list<Tail...>>
 {
     using type = type_list<Head..., Tail...>;
 };
 
-// Specialisation combining a type_list with a list of the rest
-template<typename... Head, typename... Tail>
-struct make_type_list<type_list<Head...>, Tail...>
-{
-    using type = type_list<Head..., Tail...>;
-};
-
-// Specialisation combining a regular type with a list of the rest
+// Specialisation for combining a first type with whatever comes next
 template<typename Head, typename... Tail>
 struct make_type_list<Head, Tail...>
 {
-    using type = type_list<Head, Tail...>;
+    using type = typename make_type_list<
+        typename make_type_list<Head>::type,
+        typename make_type_list<Tail...>::type
+    >::type;
 };
 
-} // namespace tools::type
+} // namespace cpptools::type_utils
 
-#endif//TOOLS__UTILITY__TYPE_IMPL__TYPE_LIST_HPP
+#endif//CPPTOOLS__UTILITY__TYPE_TRAITS_IMPL__TYPE_LIST_HPP
