455a31184d7be9d893ebdd48fa4fa11f0fdecc48
François Brachais <deqyra@gmail.com>
2024-06-29 21:51:52 +0200

    CMake fixes and new unary function

--- cpptools/utility/unary.hpp
+++ cpptools/utility/unary.hpp
@@ -1,11 +1,23 @@
 #ifndef CPPTOOLS_UTILITY_UNARY_HPP
 #define CPPTOOLS_UTILITY_UNARY_HPP
 
+#include <utility>
+
 namespace tools::unary {
 
 template<typename T>
 decltype(auto) dereference(T&& v) {
     return *v;
+}
+
+template<typename T, typename U>
+std::pair<U, T> reverse_pair(const std::pair<T, U>& p) {
+    return std::make_pair(p.second, p.first);
+}
+
+template<typename T, typename U>
+std::pair<U, T> reverse_pair(std::pair<T, U>&& p) {
+    return std::make_pair(std::move(p.second), std::move(p.first));
 }
 
 } // namespace tools::unary
