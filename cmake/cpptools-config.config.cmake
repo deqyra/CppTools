870b38cc25b869feb57ecda3f39020df20ab7f12
François Brachais <deqyra@gmail.com>
2024-07-29 23:51:11 +0200

    Export CppTools module on install, append path to CMake module path on import

--- cmake/cpptools-config.config.cmake
+++ cmake/cpptools-config.config.cmake
@@ -7,3 +7,5 @@ check_required_components( cpptools )
 if( NOT TARGET cpptools::cpptools )
     add_library( cpptools::cpptools ALIAS cpptools::cpptools_@CPPTOOLS_STATIC_OR_SHARED@ )
 endif()
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../extras")
